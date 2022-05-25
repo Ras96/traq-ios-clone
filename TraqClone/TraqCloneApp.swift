e550726b3c40abee2cc284e4d01028557611ea05
Ras <ras@pixiv.co.jp>
2022-05-25 18:08:41 +0900

    :seedling: Initial commit

---
+++ TraqClone/TraqCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraqCloneApp.swift
+//  TraqClone
+//
+//  Created by Kira Kawai on 2022/05/25.
+//
+
+import SwiftUI
+
+@main
+struct TraqCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
